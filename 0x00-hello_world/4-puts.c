10ca052917b895f05e3eaebaad5a1e7eb44b665b
yoshape <pauloluwayose@gmail.com>
2023-02-18 09:00:39 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line using the function puts
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char Mystr[10] = "Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
+}
