8c37b53bf6446137c06662106c5d5486f780c83b
yoshape <pauloluwayose@gmail.com>
2023-02-18 10:13:54 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char Mystr[10] = "Programming is like building a multilingual puzzle";
+const char *Mystr = "Programming is like building a multilingual puzzle";
 puts(Mystr);
 return (0);
 }
