6c8cbbc6bd0a940efbf883ec91a8fe21387c0202
yoshape <pauloluwayose@gmail.com>
2023-02-22 10:59:23 -0800

    8-24_hours,c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar(hour / 10);
-			_putchar(hour % 10);
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
 			_putchar(':');
-			_putchar(minute / 10);
-			_putchar(minute % 10);
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10));
 			_putchar('\n');
 		}
 	}
