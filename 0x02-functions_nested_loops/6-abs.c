e6f942192dbd2619cc0517a3d25f86b0c00347f9
yoshape <pauloluwayose@gmail.com>
2023-02-22 08:20:27 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ * @t: interger to be printed
+ * Return: The absolute value
+ */
+
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
