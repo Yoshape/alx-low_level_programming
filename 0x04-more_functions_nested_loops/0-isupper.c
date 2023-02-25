d254a0a8b3034f8ac0149abb85fff3f2e06ca2e6
yoshape <pauloluwayose@gmail.com>
2023-02-24 16:52:26 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper case character
+ * @c: the character
+ * Return 1 if c is upper case, else 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
