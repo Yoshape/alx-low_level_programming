a44349fdd9368b63e2804de6d2601bc9c9ef839f
yoshape <pauloluwayose@gmail.com>
2023-08-18 04:25:29 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,12 +17,12 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	i = ci = 0;
+	i = j = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
 		j++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
+	conct = malloc(sizeof(char) * (i + j + 1));
 
 	if (conct == NULL)
 		return (NULL);
