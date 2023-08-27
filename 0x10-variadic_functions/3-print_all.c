11d19cb8440ab9cada6f7296329469a40a22ac2a
yoshape <pauloluwayose@gmail.com>
2023-08-27 16:23:21 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,7 +33,7 @@ void print_all(const char * const format, ...)
 					s = va_arg(args, char *);
 					if (!s)
 					{
-						s = "(nill)";
+						s = "(nil)";
 					}
 					printf("%s%s", separator, s);
 					break;
